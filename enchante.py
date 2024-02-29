deeca2ee2f2ab762f17ab912926ed8413026736b
frank-xw <24924150+frank-xw@users.noreply.github.com>
2024-02-28 20:52:15 -0500

    add parser

--- enchante.py
+++ enchante.py
@@ -5,13 +5,13 @@ from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parser import person_intel_parser, PersonIntel
 
 
-if __name__ == "__main__":
+def enchante(name: str) -> PersonIntel:
     load_dotenv()
-    print("Hello Langchain")
 
-    linkedin_profile_url = linkedin_lookup_agent(name="Andrew Ng")
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
 
     # with open("prompt_info.txt", "r") as f:
     #     information = f.read()
@@ -22,11 +22,20 @@ if __name__ == "__main__":
     Given the Linkedin information {information} \
     about a person I want you to create:
     1. A short summary
-    2. two interesting facts about them
+    2. Two interesting facts about them
+    3. A topic may interest them
+    4. Two creative icebreakers may to open a conversion with them
+    \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": (
+                person_intel_parser.get_format_instructions()
+            )
+        },
     )
 
     # TODO: try local model, llama cpp
@@ -42,3 +51,9 @@ if __name__ == "__main__":
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res["text"])
+    return person_intel_parser.parse(res["text"])
+
+
+if __name__ == "__main__":
+    print("Hello Langchain")
+    result = enchante(name="Andrew Ng")
